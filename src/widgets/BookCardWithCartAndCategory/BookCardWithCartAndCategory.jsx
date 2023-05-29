d5330bd43ee5461d5d0a229cd042976ed5b6806d
Максим Расторгуев <frontimax@yandex.ru>
2023-05-29 13:29:33 +0400

    added add book in cart

--- src/widgets/BookCardWithCartAndCategory/BookCardWithCartAndCategory.jsx
+++ src/widgets/BookCardWithCartAndCategory/BookCardWithCartAndCategory.jsx
@@ -5,31 +5,34 @@ import BookCard from '../../entities/book/ui/BookCard/BookCard';
 import styles from './BookCardWithCartAndCategory.module.scss';
 import Button from '../../shared/Button/Button';
 
-function BookCardWithCartAndCategory({
-    id,
-    name,
-    author,
-    category,
-    image, 
-    price
-}) {
-  return (
-    <div className={styles.card}>
-        <div className={styles.card_category}>
-            { category }
-        </div> 
-        <BookCard 
-            name={name} 
-            author={author} 
-            image={image} 
-            price={price}
-        />
-        <div className={styles.card_cart}>
-            <Button value='Добавить в корзину' mode='primary'/>
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../store/cartSlice';
+
+function BookCardWithCartAndCategory({id, name, author, category, image, price}) {
+
+    const dispatch = useDispatch();
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.card_category}>
+                { category }
+            </div> 
+            <BookCard 
+                name={name} 
+                author={author} 
+                image={image} 
+                price={price}
+            />
+            <div className={styles.card_cart}>
+                <Button 
+                    value='Добавить в корзину' 
+                    mode='primary'
+                    handlerClick={() =>  dispatch(addBook({id, image, name, author, price}))}
+                />
+            </div>
         </div>
-        
-    </div>
-  )
+    )
 }
+
 
 export default BookCardWithCartAndCategory;
