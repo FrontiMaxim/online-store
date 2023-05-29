40f59521b93bea08588c16c165e24a77fb7e2991
Максим Расторгуев <frontimax@yandex.ru>
2023-05-29 13:25:27 +0400

    added package source-map-loader

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,12 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-            }
+            },
+            {
+                test: /\.m?js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
         ],
     }
 };
