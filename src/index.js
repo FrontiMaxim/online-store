e8b0a7ea0beacfddba02f731daaf1cd05c6d9412
Максим Расторгуев <frontimax@yandex.ru>
2023-05-29 13:28:57 +0400

    added providers for redux and router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 import App from './App';
 
+import PageBooks from './pages/PageBooks/PageBooks';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageBooks />
+    },
+    {
+      path: "/cart",
+      element: <div>Корзина</div>,
+    },
+    {
+      path: "/order",
+      element: <div>Оформление заказа</div>,
+    }
+]);
+
 const root = createRoot(document.getElementById('root')); 
-root.render(<App/>);
+root.render(
+    <Provider store={store}>
+        <RouterProvider router={router}>
+            <App/>
+        </RouterProvider>
+    </Provider>
+);
