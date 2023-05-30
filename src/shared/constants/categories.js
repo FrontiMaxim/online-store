13cedb73a95fb69c86112c69af8e02562b34e08d
Максим Расторгуев <frontimax@yandex.ru>
2023-05-30 21:58:06 +0400

    added constants

---
+++ src/shared/constants/categories.js
@@ -0,0 +1,8 @@
+const CATEGORIES = {
+    NOVEL: 'роман',
+    FANTASTIC: 'фантастика',
+    CLASSIC: 'классика',
+    EDUCATIONAL: 'образование'
+};
+
+export default CATEGORIES;
---
+++ src/shared/constants/typeSort.js
@@ -0,0 +1,7 @@
+const TYPE_SORT = {
+    BY_PRICE: 'byPrice',
+    BY_NAME: 'byName',
+    NO_SORT: 'noSort' 
+};
+
+export default TYPE_SORT;
