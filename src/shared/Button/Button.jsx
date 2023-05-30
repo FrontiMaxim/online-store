dbdaf5a3a8e26741da2bb3d2c6c80e7d7cdfe032
Максим Расторгуев <frontimax@yandex.ru>
2023-05-30 21:57:38 +0400

    added type in props

--- src/shared/Button/Button.jsx
+++ src/shared/Button/Button.jsx
@@ -4,9 +4,10 @@ import styles from './Button.module.scss';
 
 import cn from 'classnames';
 
-function Button({value, mode = 'default', handlerClick}) {
+function Button({value, handlerClick, type='button', mode = 'default'}) {
   return (
     <button 
+        type={type}
         onClick={handlerClick}
         className={cn(styles.button, {
             [styles.primary]: mode === 'primary',
