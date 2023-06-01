7cc16abff3e6555b88db1c2c05b34accb89a642e
Максим Расторгуев <frontimax@yandex.ru>
2023-06-01 13:35:16 +0400

    change BankCard on widget and added inputs

---
+++ src/widgets/BankCard/BankCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './BankCard.module.scss';
+
+import InputNumberBankCard from '../../shared/InputNumberBankCard/InputNumberBankCard';
+import InputDateBankCard from '../../shared/InputDateBankCard/InputDateBankCard';
+import InputCVVBANKCard from '../../shared/InputCVVBANKCard/InputCVVBANKCard';
+
+const BankCard = ({register, nameFieldNumber, nameFieldDate, nameFieldCVV, errors}) => {
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.head}>
+                <span>BANK</span>
+                <span>CARD</span>
+            </div>
+            <div className={styles.card_wrapper_input}>
+                <div className={styles.card_input_number}>
+                    <span className={styles.card_input_title}>Номер карты</span>
+                    <InputNumberBankCard 
+                        register={register} 
+                        name={nameFieldNumber} 
+                        error={errors[nameFieldNumber]}
+                    />
+                </div>
+                <div className={styles.card_input_date}>
+                    <span className={styles.card_input_title}>Истечение срока</span>
+                    <InputDateBankCard
+                        register={register} 
+                        name={nameFieldDate} 
+                        error={errors[nameFieldDate]}
+                    />
+                </div>
+                <div className={styles.card_input_cvv}>
+                    <span className={styles.card_input_title}>CVV</span>
+                    <InputCVVBANKCard 
+                        register={register} 
+                        name={nameFieldCVV} 
+                        error={errors[nameFieldCVV]}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BankCard;
