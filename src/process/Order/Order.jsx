78fc1168296b0686e5206e89d1e5fe59fd053f89
Максим Расторгуев <frontimax@yandex.ru>
2023-06-03 16:05:28 +0400

    added 3 stages

--- src/process/Order/Order.jsx
+++ src/process/Order/Order.jsx
@@ -4,20 +4,33 @@ import styles from './Order.module.scss';
 
 import FormWithBankCard from '../../widgets/FormWithBankCard/FormWithBankCard';
 import { useState } from 'react';
+import FormGeolacation from '../../widgets/FormGeolacation/FormGeolacation';
+import { useEffect } from 'react';
+import FormPersonalData from '../../widgets/FormPersonalData/FormPersonalData';
 
 const Order = () => {
 
     const [stage, setStage] = useState(1);
 
+    useEffect(() => {
+        if (stage === 0) {
+            console.log('Заказ оформлен');
+        }
+    }, [stage]);
 
     return (
         <div className={styles.order}>
+            
             {
-                stage === 1 && <FormWithBankCard setStage={setStage}/>
+                stage === 1 && <FormPersonalData setStage={setStage} numberStage={2} />
             }
             {
-                stage === 2 && <div>Данные пользователя</div>
+                stage === 2 && <FormGeolacation setStage={setStage} numberStage={3} />
             }
+            {
+                stage === 3 && <FormWithBankCard setStage={setStage} numberStage={0}/>
+            }
+            
         </div>
     )
 }
