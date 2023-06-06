d4d6b6f7a0d1ce9d71ae3d5b1ae1073fbaff07fc
Максим Расторгуев <frontimax@yandex.ru>
2023-06-06 11:50:26 +0400

    added additional props

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -4,14 +4,20 @@ import styles from './Button.module.scss';
 
 import cn from 'classnames';
 
-function Button({value, handlerClick, type='button', mode = 'default'}) {
+function Button({value, 
+                 handlerClick, 
+                 type='button', 
+                 mode = 'default', 
+                 borderRadius = true
+                }) {
   return (
     <button 
         type={type}
         onClick={handlerClick}
         className={cn(styles.button, {
             [styles.primary]: mode === 'primary',
-            [styles.default]: mode === 'default'
+            [styles.default]: mode === 'default',
+            [styles.border_radius]: borderRadius
         })}
     >
         {value}
